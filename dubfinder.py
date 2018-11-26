d9f77a9199123bfc79aa43f846dfd2e34d56d165
finn <serafinn1993@gmail.com>
2018-11-26 09:24:06 +0700

    make refactor

--- dubfinder.py
+++ dubfinder.py
@@ -1,119 +1,139 @@
+#!/usr/bin/env python3
+
+import re
 import os
 import sys
 import time
 import hashlib
-import _thread as thread
-from queue import Queue, Empty
+import argparse
 from collections import defaultdict
 
-def show_progress(total, processed):
-  to_Mb = lambda v: '%.2f Mb' % (v / 10**6)
-  steps_count = 50
-  progress = dubs.processed // (dubs.total // steps_count)
-  perc = '%s/%s' % (to_Mb(total), to_Mb(processed))
-  sys.stdout.write('\rProgress: |%s%s| %s' % ('=' * progress, '-' * (steps_count - progress), perc.rjust(30)))
-  sys.stdout.flush()
+class SizeParser(object):
+    SUFIXES = {'K': 10**3, 'M': 10**6, 'G': 10**9, 'T': 10**12}
 
+    @classmethod
+    def to_bytes(cls, size):
+      sufixes = cls.SUFIXES.keys()
+      base, factor = re.match(r'(\d+)?(%s)?' % '|'.join(sufixes), str(size), re.I).groups()
+      try:
+        base = int(base)
+        factor = factor and cls.SUFIXES[factor.upper()] or 1 
+      except (TypeError, ValueError, KeyError):
+        raise ValueError('Incorrect value?')
+      
+      return base * factor
+      
 class DubFinder(object):
   MODE_FAST = 'fast'
   MODE_ACCURACY = 'accuracy'
 
   def __init__(self, path, **kwargs):
-    self._processed = 0
-    self._total = 0
-    self._files_list = []
-
+    self.finded = []
+    
     self.path = path
-    self.mode = kwargs.get('mode', 'fast')
-    self.block_size = self._get_bytes(kwargs.get('block_size', '1K'))
-    self.min_size = self._get_bytes(kwargs.get('min_size', '0'))
-    self.max_size = self._get_bytes(kwargs.get('max_size')) if 'max_size' in kwargs else float('inf')
-    self.ext = kwargs.get('ext', '')
+    val_or_default = lambda name, default: kwargs.get(name) or default
+    self.mode = val_or_default('block_size', self.MODE_FAST)
+    self.block_size = SizeParser.to_bytes(val_or_default('block_size', '1M'))
+    self.min_size = SizeParser.to_bytes(val_or_default('min_size', 0))
+    self.max_size = SizeParser.to_bytes(kwargs.get('max_size')) if kwargs.get('max_size') else float('inf')
+    self.ext = val_or_default('ext', [])
 
-    self._read_path()
-
-  def _get_bytes(self, size):
-      sufixes = {'K': 10**3, 'M': 10**6, 'G': 10*9, 'T': 10**12}
-      factor = 1
-      if not size.isdecimal():
-        sufix = size[-1:].upper()
-        size = size[:-1]
-        print(sufix == 'M', sufixes.keys(), sufix not in sufixes.keys())
-        if sufix not in sufixes:
-          raise ValueError('Incorrect format')
-        factor = sufixes[sufix]
-
-      try:
-        size = int(size)
-      except ValueError:
-        raise ValueError('Incorrect format')
-
-      return size * factor
+  def process_files(self):
+    gropped = defaultdict(list)
+    for line in self._read_path():
+      gropped[self._get_file_digest(line['file_path'])].append(line)
+      yield line['file_path']
+    self.finded = filter(lambda v: len(v) > 1, gropped.values())
 
   def _read_path(self):
     for root, _, files in os.walk(self.path):
         for f in files:
           file_path = os.path.join(root, f)
           file_size = os.path.getsize(file_path)
-          if self.min_size < file_size < self.max_size:
-            self._files_list.append({'file_path': file_path, 'size': file_size})
-            self._update_total(file_size)
+          correct_size = self.min_size < file_size < self.max_size
+          correct_ext = os.path.splitext(file_path)[1].lstrip('.') in self.ext if self.ext else True
+          if correct_size and correct_ext:
+            yield {'file_path': file_path, 'size': file_size}
 
-  def find(self):
-    self._processed = 0
-    groped = defaultdict(list)
-    for line in self._files_list:
-      show_progress(self._total, self._processed)
-      groped[self._get_hash(line['file_path'])].append(line)
-      self._update_processed(line['size'])
-    groped = filter(lambda v: len(v) > 1, groped.values())
-    return groped
-
-  def _file_read(self, file_path):
-    with open(file_path, 'rb') as reader:
-      data = reader.read(self.block_size)
-      while data:
-        data = reader.read(self.block_size)
-        yield data
-        if self.mode == self.MODE_FAST:
-          break
-
-  def _get_hash(self, file_path):
+  def _get_file_digest(self, file_path):
     hs = hashlib.md5()
     for data in self._file_read(file_path):
       hs.update(data)
     return hs.hexdigest()
 
-  def _update_total(self, size):
-    size = min(size, self.min_size) if self.min_size and self.mode == self.MODE_FAST else size
-    self._total += size
+  def _file_read(self, file_path):
+    with open(file_path, 'rb') as reader:
+      while True:
+        data = reader.read(self.block_size)
+        yield data
+        if self.mode == self.MODE_FAST:
+          break
 
-  def _update_processed(self, size):
-    size = min(size, self.min_size) if self.min_size and self.mode == self.MODE_FAST else size
-    self._processed += size
 
-  # TODO make method eva
-  @property
-  def processed(self):
-    return self._processed
+def get_range(text_range):
+  total_range = set()
+  for range_part in map(str.strip, text_range.split(',')):
+    range_part = list(map(parse_int, range_part.split('-')))
+    if len(range_part) == 0 or len(range_part) > 2 or not all(range_part):
+      return None
+    if len(range_part) == 1:
+      total_range = total_range | set(range_part)
+    if len(range_part) == 2:
+        total_range = total_range | set(range(range_part[0], range_part[1] + 1)) 
+  return total_range
 
-  @property
-  def total(self):
-    return self._total
+def parse_int(digit):
+  try:
+    return int(digit)
+  except ValueError:
+    return None
+
+def process_user_input():
+  ui = input('Type range, ex. 1, 2, 5-7\nEnter to show next files\nq to Exit\n')
+  if ui == 'q':
+    return 'exit'
+  if ui == '':
+    return 'continue'
+  remove_range = get_range(ui)
+  if not remove_range:
+    print('Invalid input, try again')
+    return process_user_input()
+  deleted_files = []
+  for remove_num in remove_range:
+    remove_file = dubles[remove_num - 1]['file_path']
+    deleted_files.append(remove_file)
+    os.unlink(remove_file)
+  return deleted_files
 
 
 if __name__ == '__main__':
-  import time
-  start = time.time()
-  params = {'block_size': '1M', 'min_size': '10M'}
-  dubs = DubFinder('/media/mbrutus/3Q/books/', **params)
-  finded = dubs.find()
-  print('\ntime = %s' % (time.time() - start))
-  for dubles in finded:
+  parser = argparse.ArgumentParser()
+  parser.add_argument('path', help='Search directory')
+  parser.add_argument('--min_size', help='Min file size')
+  parser.add_argument('--max_size', help='Max file size')
+  parser.add_argument('--block_size', help='Size block in fast mode')
+  parser.add_argument('-e', '--ext', action='append', default=[], help='Extention list')
+  parser.add_argument('-f', '--fast', action='store_const', const=DubFinder.MODE_FAST, help='Fast mode, best for big binary files')
+  parser.add_argument('-a', '--accuracy', action='store_const', const=DubFinder.MODE_ACCURACY, help='Accuracy mode, best for text files')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Show current process file')
+  args = parser.parse_args()
+
+  params = {'mode': args.accuracy or args.fast or 'fast', 'block_size': args.block_size, 
+            'ext': args.ext, 'min_size': args.min_size, 'max_size': args.max_size}
+  dubs = DubFinder(args.path, **params)
+  for current_file in dubs.process_files():
+    if args.verbose:
+      print('Processed: %s' % current_file)
+
+  for dubles in dubs.finded:
     for num, duble in enumerate(dubles, 1):
-      print('\n%s) %s %sKb' % (num, duble['file_path'], str(duble['size'] / 1000).rjust(30)))
-    ui = input()
-    if ui == 'q':
+      print('\n%s) %s(%sKb)' % (num, duble['file_path'], str(duble['size'] / 1000)))
+
+    ui = process_user_input()
+    if ui == 'exit':
       break
-    if ui.isdecimal():
-      os.unlink(dubles[int(ui) - 1]['file_path'])
+    elif ui == 'continue':
+      continue
+    else:
+      print('Deleted:\n%s' % '\n'.join(ui))
+      input('Enter to continue')
